aeb6d7696edfeb417938f6bb0e5bc2487500e81d
master <justinbraun335@gmail.com>
2026-05-29 13:37:01 -0400

    feat: implement parser

---
+++ parser.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+# ================================================================================
+# =                                    PARSER                                    =
+# ================================================================================
+
+import argparse
+import sys
+
+import yaml
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command")
+
+    subparsers.add_parser("machines")
+
+    paths = subparsers.add_parser("paths")
+    paths.add_argument("machine")
+
+    links = subparsers.add_parser("links")
+    links.add_argument("machine")
+
+    packages = subparsers.add_parser("packages")
+    packages.add_argument("machine")
+    packages.add_argument("package_manager")
+
+    return parser.parse_args()
+
+
+def load_inventory() -> dict:
+    with open("test.yaml", "r") as f:
+        return yaml.safe_load(f)
+
+
+def get_machines(inventory: dict) -> None:
+    print(*inventory["machines"].keys())
+
+
+def get_paths(inventory: dict, machine: str) -> None:
+    if machine not in inventory["machines"]:
+        print(f"Unknown machine: {machine}", file=sys.stderr)
+        sys.exit(1)
+
+    for key, value in inventory.items():
+        if "paths" in value and machine in value["machines"]:
+            print(key, *value["paths"])
+
+
+def get_links(inventory: dict, machine: str) -> None:
+    if machine not in inventory["machines"]:
+        print(f"Unknown machine: {machine}", file=sys.stderr)
+        sys.exit(1)
+
+    for value in inventory.values():
+        if "links" in value and machine in value["machines"]:
+            for source, target in value["links"].items():
+                print(source, target)
+
+
+def get_packages(inventory: dict, machine: str, package_manager: str) -> None:
+    if machine not in inventory["machines"]:
+        print(f"Unknown machine: {machine}", file=sys.stderr)
+        sys.exit(1)
+
+    if package_manager not in inventory["machines"][machine]["packages"]:
+        print(
+            f"Invalid package manager for {machine}: {package_manager}", file=sys.stderr
+        )
+        sys.exit(1)
+
+    print(*inventory["machines"][machine]["packages"][package_manager])
+
+
+def main() -> None:
+    inventory = load_inventory()
+
+    args = parse_args()
+
+    if args.command == "machines":
+        get_machines(inventory)
+
+    if args.command == "paths":
+        get_paths(inventory, args.machine)
+
+    elif args.command == "links":
+        get_links(inventory, args.machine)
+
+    elif args.command == "packages":
+        get_packages(inventory, args.machine, args.package_manager)
+
+
+if __name__ == "__main__":
+    main()
